1d1151c7e16b7a491aaf00e684c58023d1761cd8
FlorentinG <florentin.goyens@student.uclouvain.be>
2015-09-15 01:04:51 +0200

    Commit de flo

--- repport/LAB1_1.m
+++ repport/LAB1_1.m
@@ -1,29 +1,25 @@
-function [] = LAB1_1()
-%LAB1_1
-%always stable because det(A)>0 and tr(A)<0
-close all;
+% LAB1_1.m - Script for question 1
+% Authors: Florentin Goyens and David Weicker
 
+close all;
 L = 0.1;
 C = 0.1;
 R = [0.01 0.1 1 10];
 E = 1;
 g = [0 E/L]';
-
 N = 1000;
 tfinal = [5 5 5 5];
+y = zeros(2,N+1); % Preallocation and initial condition
 
-q = zeros(2,N+1);
-
-for i=1:4
+for i=1:4 % For each value of R
     h = tfinal(i)/N;
     t = 0:h:tfinal(i);
     A = [0 1;-1/(L*C) -R(i)/L];
     for j=2:N+1
-        q(:,j) = A\((expm(A*t(j))-eye(2))*g);
+        y(:,j) = A\((expm(A*t(j))-eye(2))*g); % Using (2.26) in notes and zero initial condition
     end
     subplot(2,2,i);
-    plot(t,q); xlabel('Time (sec)'); hl=legend('q','$\hat{q}$');
+    plot(t,y); xlabel('Time (sec)'); hl=legend('y','$\hat{y}$');
     set(hl, 'Interpreter', 'latex');
 end
 
-end
